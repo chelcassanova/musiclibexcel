e67a5b77a316552484c79b5ab9d814950d972905
chelcassanova <sassychels251@gmail.com>
2019-01-26 12:23:53 -0500

    Initial commit

---
+++ musicexcelcreate.py
@@ -0,0 +1,79 @@
+import os
+import openpyxl
+from openpyxl.styles import Font
+import eyed3
+from musicexcelinit import *
+
+#Get the excel workbook and sheets
+file = openpyxl.load_workbook('musiclibrary.xlsx')
+sheets = file.get_sheet_names()
+
+#Create an instance of the music excel init class and get the current music library
+thing = MusicExcelInit()
+musicpath = thing.givedirec()
+
+
+#Create a list of all the folders in the main library
+musicfolders = os.listdir(musicpath)
+
+#The folder with the music and the sheets in the file have to be iterated through at the same time for this to work correctly
+for sheet, folder in zip(sheets, musicfolders):
+    #For every sheet, add these three column titles to the first three columns
+    currentsheet = file.get_sheet_by_name(sheet)
+
+    ft = Font
+    titlecell = currentsheet['A1']
+    titlecell.font = Font(bold=True)
+    titlecell = "Title"
+
+    artistcell = currentsheet['B1']
+    artistcell.font = Font(bold=True)
+    artistcell = "Artist"
+
+    albumcell = currentsheet['C1']
+    albumcell.font = Font(bold=True)
+    albumcell = "Album"
+
+    #Get the current folder in the Master Library folder so we can iterate through the files inside
+    currentpath = "C:/Users/sassy/Music/Master Library/" + folder
+    currentfolder = os.listdir(currentpath)
+
+    #Iterate through the files in the music folders and the three columns per sheet in the file at the same time
+    for currentfile, rowA, rowB, rowC in zip(currentfolder, currentsheet.iter_rows(min_col=1, max_col=1, min_row=2, max_row=(len(currentfolder) + 1)), currentsheet.iter_rows(min_col=2, max_col=2, min_row=2, max_row=(len(currentfolder) + 1)), currentsheet.iter_rows(min_col=3, max_col=3, min_row=2, max_row=(len(currentfolder) + 1))):
+        #Get the extension to determine if the current item is an mp3 file or a folder
+        extension = currentfile[len(currentfile) - 4:len(currentfile)]
+
+        if extension == ".mp3":
+            #Get the path for the file
+            tagpath = currentpath + "\\" + currentfile
+
+            #Use eyed3 to create the strings for the title, artist and album tag
+            tag = eyed3.load(tagpath)
+            title = tag.tag.title
+            #longesttitle = title
+
+            artist = tag.tag.artist
+            #longestartist = artist
+
+            album = tag.tag.album
+            #longestalbum = album
+
+            #Add the title to row 1, the artist to row 2 and the album to row 3
+            for cell in rowA:
+                cell.value = title
+                #Just set the column width after using the loop to get the longest stuff
+            for cell in rowB:
+                cell.value = artist
+            for cell in rowC:
+                cell.value = album
+        else:
+            #If the current item isn't an mp3 (it will be assumed that non-files are folders that are albums),
+            #add the title of the folder to row 1, and the string "ALBUM" to row 2
+            #Note that this will also assume that non-mp3 files like wma or flac are albums as well
+            #Can't do shit about that since the tagging info isn't the same for those kinds of files and eyed3 can't be used
+            for cell in rowA:
+                cell.value = currentfile
+            for cell in rowB:
+                cell.value = "ALBUM"
+
+file.save('musiclibrary.xlsx')
---
+++ musicexcelinit.py
@@ -0,0 +1,55 @@
+import os
+from openpyxl import *
+from tkinter import filedialog, Tk
+
+class MusicExcelInit:
+    def __init__(self):
+        #Load the file and print the sheet names
+        self.file = load_workbook('musiclibrary.xlsx')
+        self.sheets = self.file.get_sheet_names()
+
+        #Delete the current sheets in the workbook (except for one, which will be removed at the end)
+        for x in range(1, len(self.sheets)):
+            sheet = self.file.get_sheet_by_name(self.sheets[x])
+            self.file.remove_sheet(sheet)
+
+    def getdirs(self):
+        #Ask for the relevant library while hiding the Tk main window
+        Tk().withdraw()
+        self.musicpath = filedialog.askdirectory(title="Choose the music library...")
+
+        #Try to make a list of the items in the chosen directory and throw an
+        #exception if the user cancels
+        try:
+            self.direc = (os.listdir(self.musicpath))
+        except FileNotFoundError:
+            print("No directory was selected, please try again.")
+            quit()
+
+        #Create a list for folders in the directory and check the directory to compare files and folders
+        folders = []
+        for item in self.direc:
+            extension = item[len(item) - 4:len(item)]
+            if extension[0] != '.':
+                folders.append(item)
+
+    def createsheets(self):
+        #Create the sheets for the respective folders
+        for folder in folders:
+            file.create_sheet(folder)
+        print(file.get_sheet_names())
+
+        #Delete the first sheet in the workbook since it'll either be a default sheet or a duplicate sheet
+        sheets = self.file.get_sheet_names()
+        first_sheet = self.file.get_sheet_by_name(sheets[0])
+        file.remove_sheet(first_sheet)
+        file.save('musiclibrary.xlsx')
+
+    def givedirec(self):
+        """Return the directory of the music library"""
+        Tk().withdraw()
+        self.musicpath = filedialog.askdirectory(title="Choose the music library...")
+        return self.musicpath
+
+    def __str__(self):
+        return "python is fucking GOAT"
